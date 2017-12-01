5b0d4b8a9a3c0a727021c2b5c5d1265e03a41da4
Léo DESIGAUX <leodesigaux@gmail.com>
2017-12-01 04:35:53 +0100

    Refactoring

--- src/js/components/Wallet.js
+++ src/js/components/Wallet.js
@@ -1,31 +1,31 @@
 class Wallet {
-   constructor(name, defaultBalance, countId) {
-      this.name = name
-      this.balance = defaultBalance
-      this.counters = document.querySelectorAll(countId)
-      this.render()
-   }
-   add(num) {
-      this.balance += num
-      this.render()
-   }
-   remove(num) {
-      this.add( num*-1 )
-   }
-   canAdd(num) {
-      return this.balance - num > 0
-   }
-   buy(num){
-      if(this.balance - num > 0){
-         this.remove(num);
-         return true;
-      }
-      return false;
-   }
-   render() {
-      this.counters.forEach((item) => {
-         item.innerHTML = this.balance
-      });
-   }
+  constructor(name, defaultBalance, countId) {
+    this.name = name
+    this.balance = defaultBalance
+    this.counters = document.querySelectorAll(countId)
+    this.render()
+  }
+  add(num) {
+    this.balance += num
+    this.render()
+  }
+  remove(num) {
+    this.add(num * -1)
+  }
+  canAdd(num) {
+    return this.balance - num > 0
+  }
+  buy(num) {
+    if (this.balance - num > 0) {
+      this.remove(num);
+      return true;
+    }
+    return false;
+  }
+  render() {
+    this.counters.forEach((item) => {
+      item.innerHTML = this.balance
+    });
+  }
 }
-export { Wallet }
+export default Wallet
--- src/js/components/Worker.js
+++ src/js/components/Worker.js
@@ -1,74 +1,56 @@
 class Worker {
-   // instancie un worker. il travaille sur un wallet, 
-   //possède un tableau de boutons d'upgrade et un objet de conf permettant de savoir quel bouton augmente quelle upgrade.
-   // il est également envisagé de décrire les couts avec des wallets. ceci entrainerait bcp de complexité dans la conf
-   constructor(wallet, buttonId, lvlDisplayId, prodDisplayId, ) {
-   	this.wallet = wallet;
+  // instancie un worker. il travaille sur un wallet,
+  //possède un tableau de boutons d'upgrade et un objet de conf permettant de savoir quel bouton augmente quelle upgrade.
+  // il est également envisagé de décrire les couts avec des wallets. ceci entrainerait bcp de complexité dans la conf
+  constructor(wallet, buttonId, lvlDisplayId, prodDisplayId) {
+    this.wallet = wallet;
+    this.ratio = 1;
+    this.upgradeRatio = 1.1;
+    this.base = 1
+    this.upgradeCost = 1;
+    this.upgradeCostIncrease = 1;
+    this.lvl = 0;
+    this.button = document.querySelector(buttonId);
+    this.lvlDisplay = document.querySelector(lvlDisplayId);
+    this.prodDisplay = document.querySelector(prodDisplayId);
+    this.initGUI();
+    /**
+          ratio = %age
+          number => rez = multicatif
+    	    gatherSpeed =
 
-   	this.ratio = 1;
-   	this.upgradeRatio =1.1;
-   	this.base = 1
-   	this.upgradeCost = 1;
-   	this.upgradeCostIncrease = 1;
-   	this.lvl = 0;
+          afkRatio [0,1]
 
-
-
-   	this.button = document.querySelector(buttonId);
-   	this.lvlDisplay = document.querySelector(lvlDisplayId);
-   	this.prodDisplay = document.querySelector(prodDisplayId);
-   	this.initGUI();
-
-      /** 
-      ratio = %age
-      number => rez = multicatif
-	  gatherSpeed = 
-
-      afkRatio [0,1]
-
-
-      **/
-
-      // TODO plusieurs bouton
-      // differents wallets différents pour le cout des upgrades 
-      // un fichier de conf qui permets de mettre des valeurs par défaut ??? 
+          TODO plusieurs bouton
+          differents wallets différents pour le cout des upgrades
+          un fichier de conf qui permets de mettre des valeurs par défaut ???
+    **/
   }
   initGUI() {
-
-
-  	this.button.addEventListener('click', (evt) => {
-      	// conf
-      	console.log("on a cliqué")
-      	if(this.wallet.buy(this.upgradeCost)){
-      		this.upgrade();
-      		console.log("upgrade called")
-      	}
-
-
-      })
-  	
+    this.button.addEventListener('click', (evt) => {
+      if (this.wallet.buy(this.upgradeCost)) {
+        this.upgrade();
+      }
+    })
   }
-  upgrade(){
-  	this.ratio *= this.upgradeRatio;
-  	this.upgradeCost += this.upgradeCostIncrease;
-  	this.upgradeCostIncrease *= this.upgradeRatio;
-  	this.lvl++;
-  	console.log("on");
-  	this.render();
+  upgrade() {
+    this.ratio *= this.upgradeRatio;
+    this.upgradeCost += this.upgradeCostIncrease;
+    this.upgradeCostIncrease *= this.upgradeRatio;
+    this.lvl++;
+    this.render();
   }
-  produce(){
-
-  	this.wallet.add();
+  produce() {
+    this.wallet.add();
   }
-  calculateTotalProduction(){
-  	return this.base*this.ratio;
+  calculateTotalProduction() {
+    return this.base * this.ratio;
   }
-  render(){
-  	console.log(this.button);
-	this.lvlDisplay.innerHTML =  this.lvl;
-	this.prodDisplay.innerHTML = this.calculateTotalProduction();
-	this.button.innerHTML = this.button.attributes["data-text"].value.replace("$","-"+this.upgradeCost);
+  render() {
+    console.log(this.button);
+    this.lvlDisplay.innerHTML = this.lvl;
+    this.prodDisplay.innerHTML = this.calculateTotalProduction();
+    this.button.innerHTML = this.button.attributes["data-text"].value.replace("$", "-" + this.upgradeCost);
   }
-
 }
-export { Worker }
+export default Worker
