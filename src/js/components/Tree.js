0e064f4af5d5d477394629ab0e0be74d970746e3
Léo DESIGAUX <leodesigaux@gmail.com>
2017-12-01 05:21:45 +0100

    [TREE] Feedback visuel quand on gagne des nuts

--- src/js/components/Tree.js
+++ src/js/components/Tree.js
@@ -9,9 +9,11 @@ class Tree {
   }
   initGUI() {
     this.shakeButton.addEventListener('click', (evt) => {
+      evt.stopPropagation()
       this.shake()
     })
     this.pickButton.addEventListener('click', (evt) => {
+      evt.stopPropagation()
       this.pick()
     })
     this.graphic.addEventListener('webkitAnimationEnd', (evt) => {
@@ -25,10 +27,12 @@ class Tree {
   }
   pick() {
     this.wallet.add(1)
+    this.addPop(1);
   }
   result() {
     let gain = 1 + Math.ceil(Math.random() * 5)
     this.wallet.add(gain)
+    this.addPop(gain);
     let oldLabel = this.shakeButton.innerHTML;
     this.shakeButton.innerHTML = "+" + gain + " " + this.wallet.name
     setTimeout(() => {
@@ -41,6 +45,20 @@ class Tree {
   }
   enableShakeButton() {
     this.shakeButton.disabled = false;
+  }
+  addPop(num) {
+    var el = document.createElement('span');
+    el.innerHTML = "+" + num;
+    el.className = "tree-notification notification-anim"
+
+    el.style.marginLeft = Math.round(-50 + Math.random() * 100) + "px"; // Random x entre -50 et 50
+    el.style.marginTop = Math.round(Math.random() * -50) + "px"; // Random y entre 0 et -50
+
+    el.addEventListener('webkitAnimationEnd', (evt) => { //Retire le span quand l'anim est finie
+      evt.target.remove();
+    }, false)
+
+    this.graphic.parentNode.appendChild(el)
   }
 }
 export default Tree
